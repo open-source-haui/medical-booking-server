d3f1892c343a5e672225327ecd0183b48be33432
= <=>
2023-12-21 14:17:20 +0700

    fix

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 module.exports.userService = require('./user.service');
 module.exports.roleService = require('./role.service');
-module.exports.authService = require('./auth.service');
 module.exports.tokenService = require('./token.service');
+module.exports.authService = require('./auth.service');
 module.exports.emailService = require('./email.service');
 module.exports.uploadService = require('./upload.service');
 module.exports.dataService = require('./data.service');
